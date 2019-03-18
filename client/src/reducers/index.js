883c0bf50c1fb4ce1d294818e5c65cc12374397b
danxmc <danx_97@hotmail.com>
2019-03-18 15:47:58 -0600

    Add React and Redux Auth State

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import actionReducer from './actionReducer';
+import authReducer from './authReducer';
+import errorReducer from './errorReducer';
 
 export default combineReducers({
-    action: actionReducer
+    action: actionReducer,
+    auth: authReducer,
+    error: errorReducer
 })
