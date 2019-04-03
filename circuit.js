e7ac44acd7c9e05290c6eb2971067ed460bfcff6
danxmc <danx_97@hotmail.com>
2019-04-03 13:31:36 -0600

    Add raspi-io npm module

--- circuit.js
+++ circuit.js
@@ -1,18 +1,36 @@
+const config = require('config');
+
 const five = require("johnny-five");
 const EventEmitter = require('events').EventEmitter;
 
+let board;
+
+if (config.get('board') == 'raspberry') {
+    const Raspi = require('raspi-io').RaspiIO;
+    // Create board instance
+    board = new five.Board({
+        io: new Raspi()
+    });
+} else {
+    // Create board instance
+    board = new five.Board();
+}
+
 // Create emitter object to receive commands from the server
 const emitter = new EventEmitter();
-
-// Create board instance
-const board = new five.Board();
 
 // board.on
 board.on("ready", () => {
     // Connection message in the console
     console.log('ARDUINO BOARD READY STATE: TRUE');
 
-    let led = new five.Led(13);
+    let led;
+
+    if (config.get('board') == 'rasberry') {
+        led = new five.Led("P1-7");
+    } else {
+        led = new five.Led(13);
+    }
 
     emitter.on('action', (action) => {
         switch (action) {
